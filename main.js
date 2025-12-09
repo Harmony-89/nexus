3c92e6412d3bae0aaabd7fd0d2158efcb1353b27
Henry <hmwirigi45@gmail.com>
2025-12-09 10:12:46 +0300

    deleted the videos and created external links

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-var nextbutton = document.querySelector('.nxt-btn');
-var video = document.querySelector('.hero-video');
+var nextbutton = document.querySelector(".nxt-btn");
+var video = document.querySelector(".hero-video");
 
-
-
-
-var movieList = [' videos/hero-1.mp4',' videos/hero-2.mp4',' videos/hero-3.mp4',' videos/hero-4.mp4'];
+var movieList = [
+  " https://res.cloudinary.com/dvxuedo6s/video/upload/v1765261543/hero-1_azxulh.mp4",
+  " https://res.cloudinary.com/dvxuedo6s/video/upload/v1765261533/hero-2_ylfpgl.mp4",
+  " https://res.cloudinary.com/dvxuedo6s/video/upload/v1765261520/hero-3_oht0nw.mp4",
+  " https://res.cloudinary.com/dvxuedo6s/video/upload/v1765261544/hero-4_q3qj2x.mp4",
+];
 
 let index = 0;
 
-nextbutton.addEventListener('click',function(){
-    
-    index +=1;
-    video.src = movieList[index];
+nextbutton.addEventListener("click", function () {
+  index += 1;
+  video.src = movieList[index];
 
-    if(index >2){
-        index = -1;
-    }
+  if (index > 2) {
+    index = -1;
+  }
+});
 
-})
+//navigation of the pop up menu
+var shower = document.querySelector(".hum");
 
-
-    //navigation of the pop up menu
-    var shower = document.querySelector('.hum');
-
-    shower.addEventListener('click', function(){
-        document.querySelector('body').classList.toggle('active');
-    })
+shower.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("active");
+});
